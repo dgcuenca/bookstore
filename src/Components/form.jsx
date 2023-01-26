8c47824e2710109bc248c69f85d61760342b4395
diego <dgcuenca@gmail.com>
2023-01-26 11:51:14 -0500

    Add functionality for Add new book button

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -1,26 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const Form = (props) => {
+  const { title, author } = props;
+  const [newtitle, setNewTitle] = useState(title);
+  const [newAuthor, setNewAuthor] = useState(author);
+  const dispatch = useDispatch();
+
+  function validateNewbook() {
+    if (!newtitle.trim() || !newAuthor.trim()) return;
+    const newBook = { id: uuidv4(), title: newtitle, author: newAuthor };
+    dispatch(addBook(newBook));
+    setNewTitle('');
+    setNewAuthor('');
   }
 
-  render() {
-    return (
-      <form>
-        <input
-          type="text"
-          placeholder="Tittle"
-          name="title"
-        />
-        <input
-          type="text"
-          placeholder="Author"
-          name="title"
-        />
-      </form>
-    );
-  }
-}
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Tittle"
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newtitle}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        onChange={(e) => setNewAuthor(e.target.value)}
+        value={newAuthor}
+        name="author"
+      />
+      <button
+        type="button"
+        onClick={() => validateNewbook()}
+      >
+        ADD BOOK
+      </button>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+};
+
+Form.defaultProps = {
+  title: '',
+  author: '',
+};
+
 export default Form;
