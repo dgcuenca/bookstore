9e7439acae9cd1183be59dcee7b04e9542763046
diego <dgcuenca@gmail.com>
2023-01-26 13:21:12 -0500

    Fetch under construcction text

--- src/Components/categories.jsx
+++ src/Components/categories.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
 export default function Categories() {
   const dispatch = useDispatch();
+  const status = useSelector((state) => state.categories);
 
   return (
-    <button
-      type="button"
-      onClick={() => dispatch(checkStatus())}
-    >
-      Check status
-    </button>
+    <>
+      <h2>{status}</h2>
+      <button
+        type="button"
+        onClick={() => dispatch(checkStatus())}
+      >
+        Check status
+      </button>
+    </>
   );
 }
