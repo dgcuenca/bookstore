9919608b5ff4a2b4d6249075cc3fdf870fb53ec5
diego <dgcuenca@gmail.com>
2023-01-26 17:08:23 -0500

    Call API

--- src/redux/books/books.jsx
+++ src/redux/books/books.jsx
@@ -1,20 +1,26 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 // Actions
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const defaultState = [
-  {
-    id: uuidv4(),
-    title: 'Atomic Habits',
-    author: 'James Clear',
-  },
-  {
-    id: uuidv4(),
-    title: 'Emotional Intelligence',
-    author: 'Daniel Goleman',
-  },
-];
+// const defaultState = [
+//   {
+//     id: uuidv4(),
+//     title: 'Atomic Habits',
+//     author: 'James Clear',
+//   },
+//   {
+//     id: uuidv4(),
+//     title: 'Emotional Intelligence',
+//     author: 'Daniel Goleman',
+//   },
+// ];
+export const getData = createAsyncThunk('bookstore/books/GET_BOOKS', async () => fetch(
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/RWtYzZRZdkOMrjjdxWSg/books',
+).then((res) => res.json()));
+
+
 
 // Reducer
 export default function reducer(state = defaultState, action = {}) {
