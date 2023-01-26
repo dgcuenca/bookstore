761befa55dc0c33fce9ed741b9469e5ca3833af8
diego <dgcuenca@gmail.com>
2023-01-25 21:18:05 -0500

    Configure Redux store

---
+++ src/redux/configureStore.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+const rootReducer = { books: booksReducer, categories: categoriesReducer };
+
+const store = configureStore({ reducer: rootReducer });
+
+export default store;
